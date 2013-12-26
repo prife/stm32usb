596b0050b3c16119c441ecae135d34265f5ae32d
prife <goprife@gmail.com>
2013-12-26 23:59:39 +0800

    add more function

--- usb_init.c
+++ usb_init.c
@@ -50,6 +50,16 @@ void usb_cable_config(enum usb_cable_type state)
         GPIO_SetBits(USB_DISCONNECT, USB_DISCONNECT_PIN);
 }
 
+void usb_plugin()
+{
+    GPIO_ResetBits(USB_DISCONNECT, USB_DISCONNECT_PIN);
+}
+
+void usb_plugout()
+{
+    GPIO_SetBits(USB_DISCONNECT, USB_DISCONNECT_PIN);
+}
+
 /* Packet Memroy */
 #define PMA_ADDR 0x40006000L
 #if 1
@@ -116,63 +126,303 @@ int usb_endpoint_config()
 
 struct ep_buf
 {
-    u32 * ptr;
+    char buffer[1024];
     u32 len;
 };
 
-void dumphex(u32 * buf, u16 bytes)
+void dumphex(u8 * buf, u16 count)
 {
-    int i, count;
-    int value;
+    int i;
 
-    count = bytes/4 + ((bytes%4)?1:0);
-    rt_kprintf("bytes:%d, count:%d\n", bytes, count);
     for (i=0; i<count; i++)
     {
-        value = buf[i];
-#if 1
-        rt_kprintf("%02x %02x %02x %02x ",(char)value, (char)(value>>8),
-                        (char)(value>>16), (char)(value>>24));
-#else
-        rt_kprintf("%02x %02x ",(char)value, (char)(value>>8));
-#endif
-        if ((i+1)%4 == 0)
+        rt_kprintf("%02x ", buf[i]);
+        if ((i+1)%16 == 0)
             rt_kprintf("\n");
     }
-    rt_kprintf("\n");
+    if ((i+1)%16 != 0)
+        rt_kprintf("\n");
 }
 
 void get_endpoint_buf(int ep_nr, struct ep_buf *ep)
 {
-    ep->ptr = (u32*)(((u16)*EPREG_RXBUF_ADDR(ep_nr)) * 2 + PMA_ADDR);
+    int i;
+    u32 * ptr = (u32*)(((u16)*EPREG_RXBUF_ADDR(ep_nr)) * 2 + PMA_ADDR);
     ep->len = (u16)(*EPREG_RXBUF_SIZE(ep_nr)) & 0x3FF;
 
-    TRACE("EPREG_RXBUF_ADDR:%x\n", *EPREG_RXBUF_ADDR(ep_nr));
-    TRACE("EPREG_RXBUF_SIZE:%x\n", *EPREG_RXBUF_SIZE(ep_nr));
-    TRACE("ptr:%p, len:%d\n", ep->ptr, ep->len);
-    dumphex(ep->ptr, ep->len);
+    //TRACE("EPREG_RXBUF_ADDR:%x\n", *EPREG_RXBUF_ADDR(ep_nr));
+    //TRACE("EPREG_RXBUF_SIZE:%x\n", *EPREG_RXBUF_SIZE(ep_nr));
+    //TRACE("ptr:%p, len:%d\n", ptr, ep->len);
+
+    //dumphex(ptr, ep->len);
+    for (i=0; i<ep->len; i+=2)
+    {
+        *(rt_uint16_t *)(&ep->buffer[i]) = (*ptr++) & 0xFFFF;
+    }
     //dumphex((u32*)PMA_ADDR, 256);
 }
 
+struct std_device_reqeust
+{
+    rt_uint8_t bmRequestType;
+            //D7[Dir] : 0 HOST->Device
+            //          1 HOST<-Device
+            //D6-D5
+            //        :00 stadard
+            //        :01 class
+            //        :10 vendor
+            //        :11 reserved
+            //D4-D0
+            //     :00000 Device
+            //     :00001 interface
+            //     :00010 endpoint
+            //     :00011 other
+            //     :xxx00 reserved
+    rt_uint8_t bRequest;
+    rt_uint16_t rt_wValue;
+    rt_uint16_t wIndex;
+    rt_uint16_t wLength;
+};
+
+enum std_device_reqeust_type
+{
+    GET_STATUS = 0,
+    CLEAR_FEATURE = 1,
+    SET_FEATURE = 3,
+    SET_ADDRESS = 5,
+    GET_DESCRIPTOR = 6,
+    SET_DESCRIPTOR = 7,
+    GET_CONFIGURATION = 8,
+    SET_CONFIGURATION = 9,
+    GET_INTERFACE = 10,
+    SET_INTERFACE = 11,
+    SYNCH_FRAME = 12,
+};
+
+short net2host_16bit(char * buffer)
+{
+    return ((((short)buffer[1]) << 8) | buffer[0]);
+}
+
+int net2host_32bit(char * buffer)
+{
+    return ((((int)buffer[3]) << 24) |
+            (((int)buffer[2]) << 16) |
+            (((int)buffer[1]) << 8)  |
+            (((int)buffer[0])));
+}
+
+int handle_packet_setup(struct ep_buf *ep)
+{
+#define REQUEST_TYPE          ((1<<6)|(1<<5))
+#define REQUEST_TYPE_STDARD   0
+#define REQUEST_TYPE_CLASS    ((0<<6)|(1<<5))
+#define REQUEST_TYPE_VENDOR   ((1<<6)|(0<<5))
+#define REQUEST_TYPE_RESERVED ((1<<6)|(1<<5))
+
+    rt_uint8_t bmRequestType;
+    rt_uint8_t bRequest;
+    rt_uint16_t wValue;
+    rt_uint16_t wIndex;
+    rt_uint16_t wLength;
+
+    bmRequestType = ep->buffer[0];
+    bRequest = ep->buffer[1];
+    wValue = net2host_16bit(&ep->buffer[2]);
+    wIndex = net2host_16bit(&ep->buffer[4]);
+    wLength = net2host_16bit(&ep->buffer[6]);
+
+    dumphex(ep->buffer, ep->len);
+    if (bmRequestType & 0x80)
+    {
+        //OUT
+        switch (bmRequestType & REQUEST_TYPE)
+        {
+        case REQUEST_TYPE_STDARD:
+            TRACE("USB input stdard reqeust:");
+            switch(bRequest)
+            {
+            case GET_STATUS :
+                TRACE("get_status\n");
+                break;
+            case CLEAR_FEATURE :
+                TRACE("clear_feature\n");
+                break;
+            case SET_FEATURE :
+                TRACE("set_feature\n");
+                break;
+            case SET_ADDRESS :
+                TRACE("set_address\n");
+                break;
+            case GET_DESCRIPTOR :
+                TRACE("get_descriptor\n");
+                break;
+            case SET_DESCRIPTOR :
+                TRACE("set_descriptor\n");
+                break;
+            case GET_CONFIGURATION :
+                TRACE("get_configuration\n");
+                break;
+            case SET_CONFIGURATION :
+                TRACE("set_configration\n");
+                break;
+            case GET_INTERFACE :
+                TRACE("get_interface\n");
+                break;
+            case SET_INTERFACE :
+                TRACE("set_interface\n");
+                break;
+            case SYNCH_FRAME :
+                TRACE("synch_frame\n");
+                break;
+            default:
+                TRACE("unkown!\n");
+                break;
+            }
+            break;
+        case REQUEST_TYPE_CLASS:
+            TRACE("USB input Class qeuset\n");
+            break;
+        case REQUEST_TYPE_VENDOR:
+            TRACE("USB input Vendor qeuset\n");
+            break;
+        case REQUEST_TYPE_RESERVED:
+            TRACE("USB input Reserved qeuset\n");
+            break;
+        }
+
+    }
+    else
+    {
+        //IN
+        switch (bmRequestType & REQUEST_TYPE)
+        {
+        case REQUEST_TYPE_STDARD:
+            TRACE("USB output stadard qeuset\n");
+            switch(bRequest)
+            {
+            case GET_STATUS :
+                TRACE("get_status\n");
+                break;
+            case CLEAR_FEATURE :
+                TRACE("clear_feature\n");
+                break;
+            case SET_FEATURE :
+                TRACE("set_feature\n");
+                break;
+            case SET_ADDRESS :
+                TRACE("set_address\n");
+                break;
+            case GET_DESCRIPTOR :
+                TRACE("get_descriptor\n");
+                break;
+            case SET_DESCRIPTOR :
+                TRACE("set_descriptor\n");
+                break;
+            case GET_CONFIGURATION :
+                TRACE("get_configuration\n");
+                break;
+            case SET_CONFIGURATION :
+                TRACE("set_configration\n");
+                break;
+            case GET_INTERFACE :
+                TRACE("get_interface\n");
+                break;
+            case SET_INTERFACE :
+                TRACE("set_interface\n");
+                break;
+            case SYNCH_FRAME :
+                TRACE("synch_frame\n");
+                break;
+            default:
+                TRACE("unkown!\n");
+                break;
+            }
+        case REQUEST_TYPE_CLASS:
+            TRACE("USB output Class qeuset\n");
+            break;
+        case REQUEST_TYPE_VENDOR:
+            TRACE("USB output Vendor qeuset\n");
+            break;
+        case REQUEST_TYPE_RESERVED:
+            TRACE("USB output Reserved qeuset\n");
+            break;
+        }
+    }
+
+    return 0;
+}
+
+int handle_packet_out(struct ep_buf *ep)
+{
+    dumphex(ep->buffer, ep->len);
+    //TODO
+
+    return 0;
+}
+
+int handle_packet_in(struct ep_buf *ep)
+{
+    dumphex(ep->buffer, ep->len);
+    //TODO
+
+    return 0;
+}
+
 #define TAG "USB ISR:"
+static struct ep_buf ep;
 void USB_LP_CAN1_RX0_IRQHandler(void)
 {
     static __IO uint16_t istr;
     istr = _GetISTR();
     if (istr & ISTR_CTR)
     {
-        static struct ep_buf ep;
-        //int nr;
+        int ep_id;
         //int dir;
-        TRACE(TAG "ctr:\n");//Read only?
         TRACE((istr & ISTR_DIR) ? "OUT":"IN");
-        TRACE(":%d\n",istr & ISTR_EP_ID);
+        ep_id = istr & ISTR_EP_ID;
+        TRACE("endpoint:%d ctr\n", ep_id);
 
-        if (istr & ISTR_DIR)
-            get_endpoint_buf(istr & ISTR_EP_ID, &ep);
+        if (istr & ISTR_DIR) //OUT
+        {
+            int ep_value;
+            ep_value = _GetENDPOINT(ep_id);
+            get_endpoint_buf(ep_id, &ep);
+            if (ep_value & (EP_CTR_RX | EP_SETUP))
+            { //SETUP
+                if (ep_id == 0)
+                {
+                    TRACE("%d \t SETUP\n", __LINE__);
+                    handle_packet_setup(&ep);
+                }
+                else
+                {
+                    TRACE("%d \t error! setup should only to endpoint 0\n", __LINE__);
+                }
+            }
+            else
+            { //OUT
+                handle_packet_out(&ep);
+            }
+        }
+        else //IN
+        {
+            int ep_value;
+            ep_value = _GetENDPOINT(ep_id);
+            if (ep_value & (EP_CTR_RX | EP_SETUP))
+            {
+                //should never be here! I guess
+                TRACE("\t:%d SETUP\n", __LINE__);
+                handle_packet_setup(&ep);
+            }
+            else
+            {
+                handle_packet_in(&ep);
+            }
+        }
 
         //clear EndPoint RX interrupt flag
-        _SetENDPOINT((istr & ISTR_EP_ID), ~EP_CTR_RX);
+        _SetENDPOINT(ep_id, ~EP_CTR_RX);
     }
 
     if (istr & ISTR_DOVR)
@@ -231,7 +481,13 @@ deadloop
     B deadloop           //; À¿—≠ª∑
 }
 
+void reset()
+{
+    usb_plugout();
+    SystemReset();
+}
+
 #include <finsh.h>
 FINSH_FUNCTION_EXPORT(usb_init, usb init)
-FINSH_FUNCTION_EXPORT_ALIAS(SystemReset, reset, reset system);
+FINSH_FUNCTION_EXPORT(reset, reset system)
 FINSH_FUNCTION_EXPORT(dumphex, dump hex)
