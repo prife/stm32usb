383705a8a0921ba7c3e2e1f5ea9d4d88dec97997
prife <goprife@gmail.com>
2013-12-31 20:30:46 +0800

    add mouse report descriptor

--- usb_desc.c
+++ usb_desc.c
@@ -34,7 +34,37 @@ const u8 Joystick_DeviceDescriptor[] = {
 }; /* Joystick_DeviceDescriptor */
 
 /* USB Report Descriptor */
-const u8 Joystick_ReportDescriptor[] =
+static char MouseReportDescriptor[52] = {
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x02,                    // USAGE (Mouse)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x09, 0x01,                    //   USAGE (Pointer)
+    0xa1, 0x00,                    //   COLLECTION (Physical)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
+    0x29, 0x03,                    //     USAGE_MAXIMUM (Button 3)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x75, 0x05,                    //     REPORT_SIZE (5)
+    0x95, 0x01,                    //     REPORT_COUNT (1)
+    0x81, 0x03,                    //     INPUT (Cnst,Var,Abs)
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x30,                    //     USAGE (X)
+    0x09, 0x31,                    //     USAGE (Y)
+    0x09, 0x38,                    //     USAGE (Wheel)
+    0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
+    0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
+    0x75, 0x08,                    //     REPORT_SIZE (8)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x81, 0x06,                    //     INPUT (Data,Var,Rel)
+    0xc0,                          //   END_COLLECTION
+    0xc0                           // END_COLLECTION
+};
+
+static const u8 KeyboardReportDescriptor[] =
 {
     0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
     0x09, 0x06,                    // USAGE (Keyboard)
@@ -68,10 +98,10 @@ const u8 Joystick_ReportDescriptor[] =
     0x29, 0x65,                    //   USAGE_MAXIMUM (Keyboard Application)
     0x81, 0x00,                    //   INPUT (Data,Ary,Abs)
     0xc0                           // END_COLLECTION
-}; /* Joystick_ReportDescriptor */
+}; /*  KeyboardReportDescriptor */
 
 const struct descriptor ReportDesc =
-{ Joystick_ReportDescriptor, sizeof(Joystick_ReportDescriptor), };
+{ KeyboardReportDescriptor, sizeof(KeyboardReportDescriptor), };
 
 /* USB Configuration Descriptor */
 
